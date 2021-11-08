bd8f1706b20248295d15eef1732b866687ca3a37
Tamas Nepusz <ntamas@gmail.com>
2021-11-08 15:07:41 +0100

    fix limit unsigned __int32 hack for older MSVC only

--- src/plfit_mt.h
+++ src/plfit_mt.h
@@ -14,7 +14,8 @@
 #ifndef __PLFIT_MT_H__
 #define __PLFIT_MT_H__
 
-#ifdef _MSC_VER
+/* VS 2010, i.e. _MSC_VER == 1600, already has stdint.h */
+#if defined(_MSC_VER) && _MSC_VER < 1600
 #  define uint32_t unsigned __int32
 #else
 #  include <stdint.h>
